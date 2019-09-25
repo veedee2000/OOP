ed8a2d39d179feae72cf11fa224165fe5382626d
veedee <43088920+veedee2000@users.noreply.github.com>
2019-09-26 01:05:53 +0530

    Create opov_in_ex.cpp

---
+++ GENERAL/opov_in_ex.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+class test{
+  int x;
+  public:
+  test(): x{x}{}
+  friend ostream& operator << (ostream&, test&);
+  friend istream& operator >> (istream&, test&);
+};
+
+istream& operator >> (istream& input,test& obj){
+    input >> obj.x;
+    return input;
+} 
+
+ostream& operator << (ostream& output, test& obj){
+    output << obj.x;
+    return output;
+}
+
+int main(){
+    
+    test t1;
+    cin >> t1;
+    cout<< t1;
+    return 0;
+}
